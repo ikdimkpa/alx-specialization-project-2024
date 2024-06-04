65be475bc4402b6d15c1d28ce95c0412b32cb5a6
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2024-06-04 18:07:28 +0000

    Added a test case for active route and changed the test from jest to mocha

--- test/test.js
+++ test/test.js
@@ -1,5 +1,19 @@
-import connectDB from './server/config/db.js'
+import connectDB from './server/config/db'
+import { isActiveRoute } from './server/helpers/routerHelpers'
 
+/*
 test('Connection to database', () => {
 	expect(connectDB()).toBe(`Database connected: ${conn.connection.host}`);
+});
+*/
+
+
+describe('TESTING THE SERVER...', () => {
+	it('Connection to database', () => {
+		expect(connectDB()).to.be(`Database connected: ${conn.connection.host}`);
+	})
+
+	it('Checking whether the route is active', () => {
+		expect(isActiveRoute()).to.be("active" || "");
+	})
 });
